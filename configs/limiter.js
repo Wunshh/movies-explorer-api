e2f36943af9d0c4beba021967b6e30a80fa55603
Valentiva Davydova <81330170+Wunshh@users.noreply.github.com>
2021-12-27 00:02:37 +0300

    fix: изменить количество запросов к серверу и тип данных в модели movies

--- configs/limiter.js
+++ configs/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 300,
   message: 'С вашего IP сделано слишком много запросов. Повторите попытку через час.',
 });
 
